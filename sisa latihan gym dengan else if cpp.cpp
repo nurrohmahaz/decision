fd1b9aa3080132e13af6d8e83d8aa7a3b3e90ebf
nurrohmahaz <nurrohmahaz@gmail.com>
2025-02-11 08:43:42 +0700

    Add files via upload

---
+++ sisa latihan gym dengan else if cpp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string keanggotaan;
+   int latihan,Kesempatan;
+   
+   cout << "Masukkan jenis keanggotaan: ";
+   cin >> keanggotaan;
+   cout << "Masukkan jumlah latihan: ";
+   cin >>latihan;
+   
+   if (keanggotaan =="gold"){
+     Kesempatan = 20-latihan;
+     if (Kesempatan == 0){
+     cout <<"maaf anda sudah tidak bisa latihan lagi bulan ini"<<endl;
+     }else if (Kesempatan < 0){
+      cout <<"Anda sudah melebihi batas,silahkan upgrade jenis keanggotaan";
+         }else{
+             cout<<"Anda masih bisa latihan "<<Kesempatan <<" kali lagi"<<endl;
+         }
+     }else if(keanggotaan =="silver"){
+       Kesempatan = 10-latihan;
+        if (Kesempatan == 0){
+     cout <<"maaf anda sudah tidak bisa latihan lagi bulan ini"; 
+     }else if (Kesempatan < 0){
+      cout <<"Anda sudah melebihi batas,silahkan upgrade jenis keanggotaan";
+         }else{
+             cout<<"Anda masih bisa latihan "<<Kesempatan <<" kali lagi"<<endl;
+         }
+     }else if (keanggotaan =="platinum"){
+     cout <<"Anda boleh latihan sepuasnya"<<endl;  
+    } 
+  return 0;
+}
